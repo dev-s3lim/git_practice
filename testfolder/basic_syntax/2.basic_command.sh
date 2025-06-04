98b5eaba461fcb52c9376799f2db1f2199edc6c3
dev-s3lim <dev.s3lim@gmail.com>
2025-06-04 11:48:01 +0900

    Test 001

--- testfolder/basic_syntax/2.basic_command.sh
+++ testfolder/basic_syntax/2.basic_command.sh
@@ -33,5 +33,26 @@ git checkout 브랜치
 #다시 원래의 commit으로 돌아오기
 git checkout 브랜치(main등)
 
-#fetch 내 소스 코드에 반영은 안됨 (d까지 헤드 안 옮김)
-#pull 내 소스 코드에 반영됨 (d까지 헤드 옮겨버림)
+# fetch 내 소스 코드에 반영은 안됨 (d까지 헤드 안 옮김)
+# pull 내 소스 코드에 반영됨 (d까지 헤드 옮겨버림)
+# fetch는 merge 없이 origin main을 로컬 레포까지 가지고 오는것
+# pull = fetch 후 merge
+# 습관적 fetch
+
+# pull은 원격의 변경사항을 로컬로 내려받는것 (fetch + merge)
+git pull origin main
+
+# fetch 변경사항을 로컬로 가지고오되, 병합은 하지 않는것
+git fech origin main
+
+# 만약 모든 브랜치의 변경사항을 가져오려면
+git fetch --all
+
+# 커밋간의 차이점 확인
+git diff ID_A ID_B
+git diff Branch_1 Branch_2 # 브랜치간의 비교도 가능
+
+
+# 코드 수정 후 저장 > 취소 (ui discard changes)
+# Add 후 취소 > add 및 작업 취소 (ui discard changes)
+# 커밋 취소하는법
